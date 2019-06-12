fbd44a005f1315293303281d50a7fd8f322df904
Ben Cox <1038350+ind1go@users.noreply.github.com>
2019-06-12 12:42:32 +0100

    Add deployment to snapshots repo
    Signed-off-by: Ben Cox <1038350+ind1go@users.noreply.github.com>

---
+++ .travis/setup-signing.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# If this is a master build then lay out signing information
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+  openssl aes-256-cbc -K $encrypted_1e42ed2d98cb_key -iv $encrypted_1e42ed2d98cb_iv -in .travis/signingkey.asc.enc -out .travis/signingkey.asc -d
+  gpg --fast-import .travis/signingkey.asc
+  rm .travis/signingkey.asc*
+fi
