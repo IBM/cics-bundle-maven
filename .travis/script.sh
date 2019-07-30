5dd36a45858aafd5a3fab48a00506a24487f3142
Ben Cox <1038350+ind1go@users.noreply.github.com>
2019-07-30 22:56:19 +0100

    Change from maven-deploy-plugin to nexus-staging-maven-plugin
    Signed-off-by: Ben Cox <1038350+ind1go@users.noreply.github.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,7 +2,7 @@
 set -x
 
 # If this is a master build then deploy at the end
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'fix-0.0.1' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   mvn -Dstyle.color=always -B -U -P sign --settings travis-settings.xml deploy;
 # Otherwise just build and test
 else
--- .travis/setup-signing.sh
+++ .travis/setup-signing.sh
@@ -2,7 +2,7 @@
 set -x
 
 # If this is a master build then lay out signing information
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [ "$TRAVIS_BRANCH" = 'fix-0.0.1' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   openssl aes-256-cbc -K $encrypted_1e42ed2d98cb_key -iv $encrypted_1e42ed2d98cb_iv -in .travis/signingkey.asc.enc -out .travis/signingkey.asc -d
   gpg --version
   gpg --fast-import .travis/signingkey.asc
