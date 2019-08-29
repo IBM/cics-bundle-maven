56631ef1e943f9e0ccb943f9d6811594498dc95e
Stewart Francis <stewartfrancis@uk.ibm.com>
2019-08-29 11:02:05 +0100

    Updated copyrights
    Signed-off-by: Stewart Francis <stewartfrancis@uk.ibm.com>

--- cics-bundle-maven-plugin/src/it/test-reactor-debug-logging/postbuild.groovy
+++ cics-bundle-maven-plugin/src/it/test-reactor-debug-logging/postbuild.groovy
@@ -1,3 +1,16 @@
+/*-
+ * #%L
+ * CICS Bundle Maven Plugin
+ * %%
+ * Copyright (C) 2019 IBM Corp.
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
 File buildLog = new File(basedir, 'build.log')
 
 assert buildLog.text.contains("[DEBUG] Wrote resource to war-0.0.1-SNAPSHOT.war")
--- cics-bundle-maven-plugin/src/main/java/com/ibm/cics/cbmp/BuildCICSBundleMojo.java
+++ cics-bundle-maven-plugin/src/main/java/com/ibm/cics/cbmp/BuildCICSBundleMojo.java
@@ -1,5 +1,19 @@
 package com.ibm.cics.cbmp;
 
+/*-
+ * #%L
+ * CICS Bundle Maven Plugin
+ * %%
+ * Copyright (C) 2019 IBM Corp.
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
