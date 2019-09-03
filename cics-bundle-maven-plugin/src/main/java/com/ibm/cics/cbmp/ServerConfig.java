df57ad298b1adc504a94ded8933d27db8881a459
Stewart Francis <stewartfrancis@uk.ibm.com>
2019-09-03 01:28:10 +0100

    Support deploy authentication info directly in pom.xml
    Signed-off-by: Stewart Francis <stewartfrancis@uk.ibm.com>

--- cics-bundle-maven-plugin/src/main/java/com/ibm/cics/cbmp/ServerConfig.java
+++ cics-bundle-maven-plugin/src/main/java/com/ibm/cics/cbmp/ServerConfig.java
@@ -21,7 +21,25 @@ public class ServerConfig {
 	private String cicsplex;
 	private String region;
 	private URI endpointUrl;
+	private String username;
+	private String password;
 	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public void setCicsplex(String cicsplex) {
 		this.cicsplex = cicsplex;
 	}
