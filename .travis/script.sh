8ea4cfbbc62b869b8f977d2649ba2f394e7c7514
Dave Nice <dave.nice@uk.ibm.com>
2019-09-30 14:33:07 +0100

    changes to support deployment via travis
    Signed-off-by: Dave Nice <dave.nice@uk.ibm.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,6 +4,8 @@ set -x
 # If this is a master build then deploy at the end
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   mvn -Dstyle.color=always -B -U -P sign --settings travis-settings.xml deploy;
+  # TODO - after testing this, make it only publish on a release build
+  mvn site site:stage scm-publish:publish-scm;
 # Otherwise just build and test
 else
   mvn -Dstyle.color=always -B -U --settings travis-settings.xml verify;
